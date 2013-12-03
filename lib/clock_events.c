93d5dd48df9a335e10eb0e307afe6175654a497f
Sergey Markelov <s.a.markelov@gmail.com>
2013-12-03 08:57:54 -0800

    *) Still working on lib/clock_events

--- lib/clock_events.c
+++ lib/clock_events.c
@@ -7,30 +7,57 @@
 #include <logger.h>
 #endif
 
+#include "date_time.h"
 #include "clock_events.h"
+
+static int clock_events_initDates()
+{
+    ClockEvent *event = (ClockEvent *)ClockEvents;
+    for(size_t i = 0; i < CLOCK_EVENTS_SIZE; ++i, ++event) {
+        switch(event->dateType) {
+            case DATE_TYPE_MONTH_DAY:
+                for(int month = event->month; month >= JANUARY; --month) {
+                    date_time_daysInMonth();
+                }
+                break;
+            case DATE_TYPE_DAY_OF_YEAR:
+            case DATE_TYPE_MONTH_DAY_OF_WEEK:
+#ifdef PARAM_CHECKS
+            default:
+                OriginateErrorEx(ERANGE, "%d", "event->dateType [%d] is not supported", event->dateType);
+#endif
+        }
+    }
+
+    return 0;
+}
 
 //
 // @brief Calculates a number of days left to a given event
-// @param dateTime date and time from which the number of days should be calculated
+// @param dayOfYear a day of year from which the number of days should be calculated.
+//                  Usually now() in days starting from the January, 1 this year.
 // @param clockEvent a specific event to calculate the number of days to
 // @param daysToEvent the number of days will be returned here
 //
 // @returns 0 on ok
-//   EINVAL if dateTime is NULL
-//          if clockEvent is NULL
+//   EINVAL if clockEvent is NULL
 //          if daysToEvent is NULL
 //
-// @note dateTime should be normalized.
-//       clockEvent should be of an existing day, (i.e. not 34 Feb 2000).
+// @note clockEvent should be of an existing day, (i.e. not 34 Feb 2000).
 //       This function doesn't do correctness check.
 //
-int clock_events_daysToEvent(const DateTime *dateTime, const ClockEvent *clockEvent, int *daysToEvent)
+int clock_events_daysToEvent(const unsigned short dayOfYear, const ClockEvent *clockEvent, int *daysToEvent)
 {
-    NullCheck(dateTime);
     NullCheck(clockEvent);
     NullCheck(daysToEvent);
-
+    if(dayOfYear > 366 || dayOfYear == 0) {
+        OriginateErrorEx(EINVAL, "%d", "dayOfYear should be <= 366 and > 0");
+    }
 
 
     return 0;
+}
+
+int clock_events_init()
+{
 }
--- lib/clock_events.h
+++ lib/clock_events.h
@@ -5,13 +5,28 @@
 #ifndef BINARY_CLOCK_LIB_CLOCK_EVENTS_FUNCTIONS_H
 #define BINARY_CLOCK_LIB_CLOCK_EVENTS_FUNCTIONS_H
 
-#include "date_time.h"
+#define CLOCK_EVENTS_SIZE           10
 
+#define DATE_TYPE_MONTH_DAY         1
+#define DATE_TYPE_DAY_OF_YEAR       2
+#define DATE_TYPE_MONTH_DAY_OF_WEEK 3
+
+//
+// @brief ClockEvent describes an event.
+// Any event starts from a _year_. Then it may have _month_ and
+// _dayOfMonth_ or _dayOfYear_ (the first two take precedence).
+// Setting _dayOfYear_ may be needed, i.e. for the Programmer's Day
+// http://en.wikipedia.org/wiki/Programmers'_Day
+// which is 256th day of a year.
+// The rest is calculated.
+//
 typedef struct {
-    const int year;         // if set to CLOCK_EVENT_NO_START_YEAR, the event won't count years
-    const int month;
-    const int day;
-    const char *name;       // event name
+    const int year;
+    unsigned char dateType;
+    unsigned char month;
+    unsigned char dayOfMonth;
+    unsigned short dayOfYear;
+    const char *name;                   // event name
 } ClockEvent;
 
 //
@@ -21,23 +36,27 @@ typedef struct {
 //
 // @note This array is defined in lib/clock_events_personal.c
 //
-extern const ClockEvent ClockEvents[];
+// @note clock_events_init() needs to be called at the program startup and then
+// every time when the year changes.
+//
+extern const ClockEvent ClockEvents[CLOCK_EVENTS_SIZE];
 
 //
 // @brief Calculates a number of days left to a given event
-// @param dateTime date and time from which the number of days should be calculated
+// @param dayOfYear a day of year from which the number of days should be calculated.
+//                  Usually now() in days starting from the January, 1 this year.
 // @param clockEvent a specific event to calculate the number of days to
 // @param daysToEvent the number of days will be returned here
 //
 // @returns 0 on ok
-//   EINVAL if dateTime is NULL
-//          if clockEvent is NULL
+//   EINVAL if clockEvent is NULL
 //          if daysToEvent is NULL
 //
-// @note dateTime should be normalized.
-//       clockEvent should be of an existing day, (i.e. not 34 Feb 2000).
+// @note clockEvent should be of an existing day, (i.e. not 34 Feb 2000).
 //       This function doesn't do correctness check.
 //
-int clock_events_daysToEvent(const DateTime *dateTime, const ClockEvent *clockEvent, int *daysToEvent);
+int clock_events_daysToEvent(const unsigned short dayOfYear, const ClockEvent *clockEvent, int *daysToEvent);
+
+void clock_events_init();
 
 #endif
